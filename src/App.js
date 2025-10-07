7d6d04eb18f0afed09cebab19cdc02f265b2be1c
이준영 <fgh33678@gmail.com>
2025-10-07 12:50:33 +0900

    FIX: 컴포넌트 상태 확인용 App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import Header from "./components/header/Header";
+import PaperSummation from "./components/paper_summation/PaperSummation";
+import KeywordNewsList from "./components/keyword_news/KeywordNewsList";
+
 
 function App() {
   return (
@@ -11,7 +14,37 @@ function App() {
             </div>
             <div className="verticleLine"></div>
             <div className="right-container">
-
+                <KeywordNewsList/>
+                <PaperSummation
+                    title = "The Dragon Hatchling: The Missing Link between the Transformer and Models of the Brain"
+                    description = "AI가 드디어 뇌 맛을 봤다!"
+                    paper_link={"https://google.com"}
+                    is_booked = {false}
+                />
+                <PaperSummation
+                    title = "The Dragon Hatchling: The Missing Link between the Transformer and Models of the Brain"
+                    description = "AI가 드디어 뇌 맛을 봤다!"
+                    paper_link={"https://google.com"}
+                    is_booked = {true}
+                />
+                <PaperSummation
+                    title = "The Dragon Hatchling: The Missing Link between the Transformer and Models of the Brain"
+                    description = "AI가 드디어 뇌 맛을 봤다!"
+                    paper_link={"https://google.com"}
+                    is_booked = {false}
+                />
+                <PaperSummation
+                    title = "The Dragon Hatchling: The Missing Link between the Transformer and Models of the Brain"
+                    description = "AI가 드디어 뇌 맛을 봤다!"
+                    paper_link={"https://google.com"}
+                    is_booked = {false}
+                />
+                <PaperSummation
+                    title = "The Dragon Hatchling: The Missing Link between the Transformer and Models of the Brain"
+                    description = "AI가 드디어 뇌 맛을 봤다!"
+                    paper_link={"https://google.com"}
+                    is_booked = {true}
+                />
             </div>
         </div>
     </div>
