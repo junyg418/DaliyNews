4061f51a26331572a7d202dd797088ec6ebb27e9
이준영 <fgh33678@gmail.com>
2025-10-14 20:31:16 +0900

    Refactor: Header css -> styled 사용 통일

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,17 @@
-import styles from "./Header.module.css";
+import {Row} from "../../styles/layout"
+import styled from "styled-components";
 import {ReactComponent as Logo} from "../../assets/logo.svg";
 
+const StyledMainHeader = styled(Row)`
+    align-items: flex-start;
+    padding: 1%;
+    gap: 1%;
+`;
 
 function MainHeader() {
     // Event event, Alarm alerm 추가 해야한다.
     return (
-        <div className={styles.app_header}>
+        <StyledMainHeader>
             <Logo width={200}/>
             <div style={{backgroundColor: "green"}}>
                 오늘, 내일 날시 알려주거나 | 컨퍼런스 와 행사 일정 돌아가며 계속해서 보여줌 두 기능 고민 해봐야겠다.
@@ -16,7 +22,7 @@ function MainHeader() {
             <div style={{backgroundColor: "blue"}}>
                 alarm
             </div>
-        </div>
+        </StyledMainHeader>
     );
 }
 
